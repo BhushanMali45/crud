3cffa9f1f82c8f5561bf7e25019e4d10d4f757e4
bhushan <mbhushan4545@gmail.com>
2024-06-17 11:11:02 +0530

    hi

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,57 @@
 
 
 
+// $("#add_user").submit(function(event){
+//     alert("Data Inserted Successfully!");
+// })
+
+// $("#update_user").submit(function(event){
+//     event.preventDefault();
+
+//     var unindexed_array = $(this).serializeArray();
+//     var data = {}
+
+//     $.map(unindexed_array, function(n, i){
+//         data[n['name']] = n['value']
+//     })
+
+
+//     var request = {
+//         "url" : `http://localhost:3000/api/users/${data.id}`,
+//         "method" : "PUT",
+//         "data" : data
+//     }
+
+//     $.ajax(request).done(function(response){
+//         alert("Data Updated Successfully!");
+//     })
+
+// })
+
+// if(window.location.pathname == "/"){
+//     $ondelete = $(".table tbody td a.delete");
+//     $ondelete.click(function(){
+//         var id = $(this).attr("data-id")
+
+//         var request = {
+//             "url" : `http://localhost:3000/api/users/${id}`,
+//             "method" : "DELETE"
+//         }
+
+//         if(confirm("Do you really want to delete this record?")){
+//             $.ajax(request).done(function(response){
+//                 alert("Data Deleted Successfully!");
+//                 location.reload();
+//             })
+//         }
+
+//     })
+// }
+
+
+
+
+
 $("#add_user").submit(function(event){
     alert("Data Inserted Successfully!");
 })
@@ -17,7 +68,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `https://crud-q5bz.onrender.com/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -34,7 +85,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `https://crud-q5bz.onrender.com/api/users/${id}`,
             "method" : "DELETE"
         }
 
--- server/services/render.js
+++ server/services/render.js
@@ -1,10 +1,43 @@
 
+// const axios=require('axios');
+
+// exports.homeRoutes=(req,res)=>{
+//     // res.render("index",{users:"New Data"} );
+
+//     axios.get("http://localhost:3000/api/users")
+//     .then(function(response){
+//         // console.log(response)
+//         res.render('index',{users:response.data});
+//     }).catch(err=>{
+// res.send(err);
+//     })
+// }
+
+
+// exports.add_user=(req,res)=>{
+//     res.render("add_user" );
+// }
+
+
+
+// exports.update_user = (req, res) =>{
+//     axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+//         .then(function(userdata){
+//             res.render("update_user", { user : userdata.data})
+//         })
+//         .catch(err =>{
+//             res.send(err);
+//         })
+// }
+
+
+
 const axios=require('axios');
 
 exports.homeRoutes=(req,res)=>{
     // res.render("index",{users:"New Data"} );
 
-    axios.get("http://localhost:3000/api/users")
+    axios.get("https://crud-q5bz.onrender.com/api/users")
     .then(function(response){
         // console.log(response)
         res.render('index',{users:response.data});
@@ -21,7 +54,7 @@ exports.add_user=(req,res)=>{
 
 
 exports.update_user = (req, res) =>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+    axios.get('https://crud-q5bz.onrender.com/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
